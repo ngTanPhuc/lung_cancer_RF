1cf9d8ca9b524577ce767590545f948c00590f4e
ngTanPhuc <nguyentanphuc020505@gmail.com>
2025-12-04 13:24:27 +0700

    create branch xgboost

--- src/dataset.py
+++ src/dataset.py
@@ -4,10 +4,12 @@ from sklearn.model_selection import train_test_split
 
 class Dataset:
     def __init__(self):
-        self.dataset_path = os.path.join('..', 'data', 'processed', 'dataseter.csv')
+        self.dataset_path = os.path.join('..', 'data', 'processed', 'dataset.csv')
         self.dataset = pd.read_csv(self.dataset_path)
         self.features = None
         self.labels = None
+
+        print(self.dataset.head(5))
     
     def split_data(self, target_column, test_size=0.2, random_state=42, shuffle=True):
         self.features = self.dataset.drop(columns=[target_column])
