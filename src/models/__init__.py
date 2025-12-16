6ed83cdad1c93050ff2eeb08c54724268f28ca28
Bui Nhat Qui <buinhatqui011105@gmail.com>
2025-12-17 04:35:04 +0700

    Add class AdaBoostModel to init of src/models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,11 @@
 from .base_model import BaseModel
 from .decision_tree import DecisionTreeModel
 from .random_forest import RandomForestModel
+from .ada_boost import AdaBoostModel
 
 __all__ = [
     "BaseModel",
     "DecisionTreeModel",
     "RandomForestModel",
+    "AdaBoostModel"
 ]
